e37c23b0bc7ab10cf446ca5cdcfef2a2377a3571
claydoh07 <46361821+claydoh07@users.noreply.github.com>
2019-01-10 16:24:09 +0800

    RegisterWithDatabase

--- ShoppeTown-InventorySystem/ShoppeTown-InventorySystem/Classes/MyDatabase.cs
+++ ShoppeTown-InventorySystem/ShoppeTown-InventorySystem/Classes/MyDatabase.cs
@@ -47,6 +47,72 @@ namespace ShoppeTown_InventorySystem
             return usersResult;
         }
         /*---------- register ----------*/
+        public string TestRegister(string usr, string pwd)
+        {
+            string checkUsers = "";
+            try
+            {
+                con.Open();
+                string SqlSelectUsers = "SELECT * FROM tbl_users WHERE username=@u AND password=@p;";
+                MySqlCommand com = new MySqlCommand(SqlSelectUsers, con);
+                com.Parameters.AddWithValue("@u", usr);
+                com.Parameters.AddWithValue("@p", pwd);
+                com.ExecuteNonQuery();
+
+                MySqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    checkUsers = "EXISTING";
+                }
+            }
+            catch (MySqlException sql)
+            {
+                MessageBox.Show(sql.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return checkUsers;
+        }
+        //Insert password and username
+        public string Register(string usr, string pwd)
+        {
+             try
+            {
+                con.Open();
+                string SqlInsertRegister = "INSERT INTO tbl_users(" +
+                    "username, password, firstName, middleName, lastName, " +
+                    "userType, position, department, created_at, updated_at) " +
+                    "VALUES(@username, @password, @fname, @mname, @lname, " +
+                    "@usertype, @position, @department, @created_at, @updated_at);";
+                MySqlCommand com = new MySqlCommand(SqlInsertRegister, con);
+                com.Parameters.AddWithValue("@username", usr);
+                com.Parameters.AddWithValue("@password", pwd);
+                com.Parameters.AddWithValue("@fname", RegisterAutoProperty.fname);
+                com.Parameters.AddWithValue("@mname", RegisterAutoProperty.mname);
+                com.Parameters.AddWithValue("@lname", RegisterAutoProperty.lname);
+                com.Parameters.AddWithValue("@usertype", RegisterAutoProperty.usertype);
+                com.Parameters.AddWithValue("@position", RegisterAutoProperty.position);
+                com.Parameters.AddWithValue("@department", RegisterAutoProperty.department);
+                
+                com.Parameters.AddWithValue("@created_at", DateTime.Now.ToString("yyyy'-'MM'-'dd' 'HH':'mm':'ss"));
+                com.Parameters.AddWithValue("@updated_at", DateTime.Now.ToString("yyyy'-'MM'-'dd' 'HH':'mm':'ss"));
+                com.ExecuteNonQuery();
+                //  YYYY-MM-DD HH:MM:SS
+            }
+            catch (MySqlException sql)
+            {
+                MessageBox.Show(sql.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return null;
+        }
+
+
 
     }
 }
---
+++ ShoppeTown-InventorySystem/ShoppeTown-InventorySystem/nextregister.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ShoppeTown_InventorySystem
+{
+    partial class nextregister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txtUsername = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.btnLogin = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(378, 61);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(114)))), ((int)(((byte)(11)))));
+            this.label1.Location = new System.Drawing.Point(23, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 28);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Register";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(25, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 42);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Set your Username and Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPassword.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtPassword.ForeColor = System.Drawing.Color.DarkGray;
+            this.txtPassword.HintForeColor = System.Drawing.Color.Empty;
+            this.txtPassword.HintText = "Password";
+            this.txtPassword.isPassword = false;
+            this.txtPassword.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(89)))), ((int)(((byte)(5)))));
+            this.txtPassword.LineIdleColor = System.Drawing.Color.DarkGray;
+            this.txtPassword.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(119)))), ((int)(((byte)(6)))));
+            this.txtPassword.LineThickness = 3;
+            this.txtPassword.Location = new System.Drawing.Point(28, 186);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(313, 44);
+            this.txtPassword.TabIndex = 15;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsername.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtUsername.ForeColor = System.Drawing.Color.DarkGray;
+            this.txtUsername.HintForeColor = System.Drawing.Color.Empty;
+            this.txtUsername.HintText = "Username";
+            this.txtUsername.isPassword = false;
+            this.txtUsername.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(89)))), ((int)(((byte)(5)))));
+            this.txtUsername.LineIdleColor = System.Drawing.Color.DarkGray;
+            this.txtUsername.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(119)))), ((int)(((byte)(6)))));
+            this.txtUsername.LineThickness = 3;
+            this.txtUsername.Location = new System.Drawing.Point(28, 134);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(313, 44);
+            this.txtUsername.TabIndex = 14;
+            this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(127)))), ((int)(((byte)(12)))));
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(127)))), ((int)(((byte)(12)))));
+            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLogin.BorderRadius = 0;
+            this.btnLogin.ButtonText = "REGISTER";
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.DisabledColor = System.Drawing.Color.Gray;
+            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnLogin.Iconimage = null;
+            this.btnLogin.Iconimage_right = null;
+            this.btnLogin.Iconimage_right_Selected = null;
+            this.btnLogin.Iconimage_Selected = null;
+            this.btnLogin.IconMarginLeft = 0;
+            this.btnLogin.IconMarginRight = 0;
+            this.btnLogin.IconRightVisible = true;
+            this.btnLogin.IconRightZoom = 0D;
+            this.btnLogin.IconVisible = true;
+            this.btnLogin.IconZoom = 90D;
+            this.btnLogin.IsTab = false;
+            this.btnLogin.Location = new System.Drawing.Point(28, 255);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(127)))), ((int)(((byte)(12)))));
+            this.btnLogin.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(103)))), ((int)(((byte)(6)))));
+            this.btnLogin.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnLogin.selected = false;
+            this.btnLogin.Size = new System.Drawing.Size(313, 51);
+            this.btnLogin.TabIndex = 16;
+            this.btnLogin.Text = "REGISTER";
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLogin.Textcolor = System.Drawing.Color.White;
+            this.btnLogin.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // nextregister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 344);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "nextregister";
+            this.Style = MetroFramework.MetroColorStyle.White;
+            this.Load += new System.EventHandler(this.nextregister_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        public Bunifu.Framework.UI.BunifuMaterialTextbox txtPassword;
+        public Bunifu.Framework.UI.BunifuMaterialTextbox txtUsername;
+        public Bunifu.Framework.UI.BunifuFlatButton btnLogin;
+    }
+}
